e3a157f5cd82b31c830f25cc95d48e4dd62bc082
Morven Lewis-Everley <morven@i-lateral.com>
2023-06-19 11:57:24 +0100

    Update esteps for orbitor

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1173,7 +1173,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 1600, 705 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 1600, 680 }
 
 /**
  * Default Max Feed Rate (linear=mm/s, rotational=°/s)
