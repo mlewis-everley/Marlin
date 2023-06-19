7c63725885747dadca748ac5403ebb88220d30ad
Morven Lewis-Everley <morven@i-lateral.com>
2023-06-19 11:58:37 +0100

    Tweak extruder esteps

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1173,7 +1173,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 1600, 680 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 1600, 680.5 }
 
 /**
  * Default Max Feed Rate (linear=mm/s, rotational=°/s)
