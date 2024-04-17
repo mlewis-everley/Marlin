5da4fbc1da7420a8c1fbdb88e894dd5023405901
Morven Lewis-Everley <morven@i-lateral.com>
2024-04-17 23:14:55 +0100

    Add host actions and cancel objects

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -28,7 +28,7 @@
 /**
  * Marlin release version identifier
  */
-#define SHORT_BUILD_VERSION "2.1.x-SV06-R9"
+#define SHORT_BUILD_VERSION "2.1.x-SV06-R10"
 
 /**
  * Verbose version identifier which should contain a reference to the location
