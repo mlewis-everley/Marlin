3fe8b9db7928fb8343835d10449c2e689f9f5568
Morven Lewis-Everley <morven@i-lateral.com>
2024-04-17 22:36:50 +0100

    Tweak feedrates and acceleration

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -28,7 +28,7 @@
 /**
  * Marlin release version identifier
  */
-#define SHORT_BUILD_VERSION "2.1.x-SV06-R2"
+#define SHORT_BUILD_VERSION "2.1.x-SV06-R3"
 
 /**
  * Verbose version identifier which should contain a reference to the location
